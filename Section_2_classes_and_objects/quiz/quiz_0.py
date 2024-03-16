992d741397fc144dc45d7aa13e80075017968dd9
Mel-iza <meliza.caug@gmail.com>
2024-03-16 19:25:46 -0300

    moving files for better organization

--- Section_2_classes_and_objects/quiz/quiz_0.py
+++
@@ -1,8 +0,0 @@
-class Test:
-    pass 
-
-t1 = Test()
-t2 = Test()
-
-print(t1 == t2, end=' ')
-print(type(t1) == type(t2), end=' ')
--- Section_2_classes_and_objects/quiz/quiz_2.py
+++
@@ -1,8 +0,0 @@
-class Test:
-    def method1(self):
-        x = 12
-
-    def method2(self):
-        self.y = 10
-
-# y is an instance variable, x is a local variable
